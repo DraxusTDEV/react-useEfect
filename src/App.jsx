a1ac392e228b03334a18e67ea2af503693c78b6a
DraxusTDEV <draxustienda@gmail.com>
2024-02-23 21:38:00 -0500

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [counter, setcounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Cambió el counter");
+  }, [counter]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>UseEffect</h1>
+      <button onClick={() => setcounter(counter + 1)}>
+        Counter: {counter}
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
